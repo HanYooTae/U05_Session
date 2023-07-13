ab53da574c367bac2acf1751755986baa35dbe67
rlatmdgh35 <41534351+HanYooTae@users.noreply.github.com>
2023-07-13 19:55:06 +0900

    0713

---
+++ Source/U05_Session/CGameInstance.cpp
@@ -0,0 +1,43 @@
+#include "CGameInstance.h"
+#include "Global.h"
+#include "Blueprint/UserWidget.h"
+
+UCGameInstance::UCGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	CLog::Log("GameInstance::Constructor Called");
+
+	TSubclassOf<UUserWidget> menuWidgetClass;
+	CHelpers::GetClass(&menuWidgetClass, "/Game/Widgets/WB_Menu");
+	CLog::Log(menuWidgetClass->GetName());
+}
+
+void UCGameInstance::Init()
+{
+	Super::Init();
+
+	CLog::Log("GameInstance::Init Called");
+
+	//CreateWidget(this, )
+}
+
+void UCGameInstance::Host()
+{
+	CLog::Print("Host");
+	//-> Everybody Move to Play Map
+	UWorld* world = GetWorld();
+	CheckNull(world);
+
+	world->ServerTravel("/Game/Maps/Play?listen");
+}
+
+void UCGameInstance::Join(const FString& InAddress)
+{
+	//GetEngine()->AddOnScreenDebugMessage(-1, 2, FColor::Green, FString::Printf(L"Join to %s", InAddress));
+	CLog::Print("Join to " + InAddress);
+
+	//UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	//GetWorld()->GetFirstPlayerController();
+	APlayerController* controller = GetFirstLocalPlayerController();
+	CheckNull(controller);
+	controller->ClientTravel(InAddress, ETravelType::TRAVEL_Absolute);
+}
